8399bfb1c59863be84a2cbe789a27d97ccfbcf9f
Anil Surriya <anilsurriya@gmail.com>
2023-11-04 13:28:45 +0530

    fix port is str

--- leetcode/run.py
+++ leetcode/run.py
@@ -1,5 +1,5 @@
 from leetcode.scrapper import *
-from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
+from xmlrpc.server import SimpleXMLRPCServer
 
 import os
 
@@ -7,16 +7,15 @@ host = '0.0.0.0'
 fallback_port = 10000
 if 'PORT' not in os.environ:
     print(f'PORT environment variable not defined.\nUsing fallback port {fallback_port}')
-port = os.environ.get('PORT', fallback_port)
+port = int(os.environ.get('PORT', fallback_port))
 
 def serve():
-    server = SimpleJSONRPCServer((host, port))
-    
-    funcs = [ initialize, questions_solved_count, contributions, profile, total_submissions, search_question_by_name]
-    for func in funcs:
-        server.register_function(func)
+    with SimpleXMLRPCServer((host, port)) as server:
+        funcs = [ initialize, questions_solved_count, contributions, profile, total_submissions, search_question_by_name]
+        for func in funcs:
+            server.register_function(func)
 
-    server.serve_forever()
+        server.serve_forever()
 
 if __name__ == '__main__':
     serve()
